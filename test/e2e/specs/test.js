b970a73996414774ba30962eeb82c62347ddb38a
Will Wise <william.wise@siteminder.com>
2018-02-05 09:24:33 +1100

    Reworked the e2e tests with new selectors

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,24 +11,21 @@ module.exports = {
       .assert.elementPresent('[name="To"]')
       .assert.elementPresent('[name="Subject"]')
       .assert.elementPresent('[name="Message"]')
-      .assert.elementPresent('.submit button')
+      .assert.elementPresent('.actions-submit button')
       .assert.containsText('.header h1', 'New message')
 
       // Test a prevented submission
       // (Missing recipient)
-      .click('.submit button')
+      .click('.actions-submit button')
       .pause(500)
       .assert.visible('.el-message-box__wrapper')
 
       // Test a prevented submission
       // (Missing subject)
       .setValue('input[name="To"]', ['test@test.com', browser.Keys.ENTER])
-      .click('.submit button')
+      .click('.actions-submit button')
       .pause(500)
       .assert.visible('.el-message-box__wrapper')
-
-      // Test that the address fields are listing items
-      .assert.containsText('.address-selection', 'test@test.com')
 
       .end()
   },
